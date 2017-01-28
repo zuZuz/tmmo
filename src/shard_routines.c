42eda6ba72b449656b9c98f241d201fb28470b54
Aidar <zreacta@gmail.com>
2017-01-28 15:21:05 +0500

    Headers for shard routines added

---
+++ src/shard_routines.c
@@ -0,0 +1,11 @@
+#include <pthread.h>
+
+void* shard_master_routine(void* arg)
+{
+	return NULL;
+}
+
+void* shard_slave_routine(void* arg)
+{
+	return NULL;
+}
---
+++ src/shard_routines.h
@@ -0,0 +1,10 @@
+#ifndef _SHARD_ROUTINES_H_
+#define _SHARD_ROUTINES_H_
+
+void* 
+shard_master_routine(void* arg);
+
+void* 
+shard_slave_routine(void* arg);
+
+#endif
