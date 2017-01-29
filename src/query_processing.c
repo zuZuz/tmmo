1dc0ba87894c835b79824fbd8ad2f3ac0cb1dce6
PrestoV <postem256@gmail.com>
2017-01-30 02:51:09 +0300

    Query processing implementation added

--- src/query_processing.c
+++ src/query_processing.c
@@ -1,5 +1,48 @@
-//
-// Created by Роман on 29.01.17.
-//
-
 #include "query_processing.h"
+#include "threadpool.h"
+
+void* query_processing(void *arg)
+{
+    threadpool_t *thpool = threadpool_create(THREADS_CNT);
+
+    query_processing_start( threadpool_get_jqueue(thpool), (qprocess_args_t*)arg);
+
+    threadpool_destroy(thpool);
+
+    return NULL;
+}
+
+static void query_processing_start(jqueue_t *jqueue, qprocess_args_t *args)
+{
+    msg_t *message;
+    queue_t *in_queue = args -> in;
+
+    while( !(args->is_terminated) )
+    {
+        pthread_mutex_lock( &(in_queue->mutex) );
+
+
+        while(queue_is_empty(in_queue) && !(args->is_terminated) )
+            pthread_cond_wait( &(in_queue->cond), &(in_queue->mutex) );
+
+        queue_dequeue(in_queue, &message);
+
+
+        pthread_mutex_unlock( &(in_queue->mutex) );
+
+        if(message != NULL)
+            jqueue_add_job(jqueue, query_processing_new, message, args -> out);
+
+    }
+
+}
+
+static void query_processing_new(msg_t *message, queue_t *out_queue)
+{
+    char *data = message->body + sizeof( message_type_t );
+    size_t length = message->len - sizeof( message_type_t );
+
+    //TODO: process
+
+    queue_enqueue(out_queue, message);
+}
--- src/query_processing.h
+++ src/query_processing.h
@@ -1,8 +1,24 @@
-//
-// Created by Роман on 29.01.17.
-//
+#include "queue.h"
+#include "network.h"
 
-#ifndef TMMO_QUERY_PROCESSING_H
-#define TMMO_QUERY_PROCESSING_H
+#ifndef QUERY_PROCESSING_H
+#define QUERY_PROCESSING_H
 
-#endif //TMMO_QUERY_PROCESSING_H
+#define THREADS_CNT 5
+
+typedef enum message_type
+{
+    text = 0
+
+} message_type_t;
+
+typedef struct qprocess_args
+{
+    queue_t *in;
+    queue_t *out;
+    bool *is_terminated;
+} qprocess_args_t;
+
+void* query_processing(void *arg);
+
+#endif //QUERY_PROCESSING_H
