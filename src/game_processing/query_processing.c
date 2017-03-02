b02c4f7360613b28bd32533183e00ad6797bfbce
PrestoV <postem256@gmail.com>
2017-03-02 21:23:46 +0300

    query processing refactored

--- src/game_processing/query_processing.c
+++ src/game_processing/query_processing.c
@@ -1,6 +1,5 @@
 #include "query_processing.h"
 #include "game_functions.h"
-#include "threadpool.h"
 #include "shared_funcs.h"
 
 static bool query_text(msg_t *msg)
@@ -19,7 +18,7 @@ static bool query_text(msg_t *msg)
         return false;
 }
 
-static void query_processing_new(void *message, void *out_queue)
+void query_processing_new(void *message, void *out_queue)
 {
     bool have_asnw;
 
@@ -39,41 +38,4 @@ static void query_processing_new(void *message, void *out_queue)
         queue_enqueue((queue_t *) out_queue, message);
         pthread_cond_signal(&((queue_t *) out_queue)->cond);
     }
-}
-
-static void query_processing_start(jqueue_t *jqueue, qprocess_args_t *args)
-{
-    msg_t *message;
-    queue_t *in_queue = args -> in;
-
-    while( !*(args->is_terminated) )
-    {
-        pthread_mutex_lock( &(in_queue->mutex) );
-
-
-        while(queue_is_empty(in_queue) && !*(args->is_terminated) )
-            pthread_cond_wait( &(in_queue->cond), &(in_queue->mutex) );
-
-
-        pthread_mutex_unlock( &(in_queue->mutex) );
-
-        queue_dequeue(in_queue, (void*)&message);
-
-        if(message != NULL)
-            jqueue_add_job(jqueue, query_processing_new, message, args -> out);
-
-    }
-
-}
-
-
-void* query_processing(void *arg)
-{
-    threadpool_t *thpool = threadpool_create(THREADS_CNT);
-
-    query_processing_start( threadpool_get_jqueue(thpool), (qprocess_args_t*)arg);
-
-    threadpool_destroy(thpool);
-
-    return NULL;
 }
--- src/game_processing/query_processing.h
+++ src/game_processing/query_processing.h
@@ -1,5 +1,5 @@
 /*
- * Processing requests from the queue "in" and adding the responses to the queue "out".
+ * Processing requests with adding the responses to the queue "out".
  */
 
 #include "../server/queue.h"
@@ -8,16 +8,6 @@
 #ifndef QUERY_PROCESSING_H
 #define QUERY_PROCESSING_H
 
-//the number of threads in the thread pool
-#define THREADS_CNT 5
-
-typedef struct qprocess_args
-{
-    queue_t *in;
-    queue_t *out;
-    bool *is_terminated;
-} qprocess_args_t;
-
-void* query_processing(void *arg);
+void query_processing_new(void *message, void *out_queue);
 
 #endif //QUERY_PROCESSING_H
