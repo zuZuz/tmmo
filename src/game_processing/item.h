b1b34117341d19afd77b4995c10d319a3e2a74bb
PrestoV <postem256@gmail.com>
2017-03-28 00:43:49 +0300

    Add new message types processing

--- src/game_processing/item.h
+++ src/game_processing/item.h
@@ -1,7 +1,14 @@
 #include "character.h"
+#include "../map&builds_gen/buildgen.h"
 
 #ifndef ITEM_H
 #define ITEM_H
+
+typedef struct item_building
+{
+    building_t *building;
+    items_t *items;
+} item_building_t;
 
 item_t* item_new(item_type_t type, char *name, characteristics_t characteristics, int strength);
 
--- src/game_processing/query_processing.c
+++ src/game_processing/query_processing.c
@@ -1,6 +1,9 @@
+#include <memory.h>
 #include "query_processing.h"
 #include "game_functions.h"
 #include "shared_funcs.h"
+#include "character.h"
+#include "game_main.h"
 
 static queue_t* out_queue;
 
@@ -9,7 +12,14 @@ static void query_text(msg_t *msg, msg_t **reply_msg)
     char *pos = msg->body;
     char *func_name;
 
-    func_name = get_word(&pos);
+    if(strlen(character_get_by_addr( &( msg->addr ) )->name) == 0)
+    {
+        strcpy(character_get_by_addr( &( msg->addr ) )->name, msg->body);
+        character_get_by_addr( &( msg->addr ) )->aggression = true;
+        func_name = "hello";
+    }
+    else
+        func_name = get_word(&pos);
 
     if(func_name != NULL)
     {
@@ -22,6 +32,9 @@ void query_processing_new(void *message)
     bool have_asnw = false;
 
     msg_t *reply_msg = NULL;
+    struct sockaddr_in *addr;
+
+    printf("player %s:%d\n", inet_ntoa(((msg_t*)message)->addr.sin_addr), ntohs(((msg_t*)message)->addr.sin_port));
 
     switch( ((msg_t*)message) -> type )
     {
@@ -31,13 +44,47 @@ void query_processing_new(void *message)
         case conn_test: have_asnw = true;
             break;
 
-        default: return;
+        case logged_in:
+            addr = malloc( sizeof(struct sockaddr_in) );
+            *addr = ((msg_t*)message)->addr;
+            character_add(game_get_characters(), character_new(76, 134, "", human, 1, 0, true, addr));
+
+            reply_msg = msg_init(NULL);
+            (reply_msg)->addr = ((msg_t*)message)->addr;
+            strcpy((reply_msg)->key, ((msg_t*)message)->key);
+
+            (reply_msg)->type = main_msg;
+            (reply_msg)->len = (size_t)sprintf((reply_msg)->body, "What is your name?") + 1;
+
+            break;
+
+        case timed_out:
+            character_remove(game_get_characters(), character_get_by_addr( &( ((msg_t*)message)->addr ) ));
+            break;
+
+        case map_update:
+            gfunc_map((msg_t*)message, &reply_msg, NULL);
+            break;
+
+        case char_info:
+            gfunc_getinfo((msg_t*)message, &reply_msg, NULL);
+            break;
+
+        case online_list:
+            have_asnw = true;
+            break;
+
+        case chat_msg:
+            have_asnw = true;
+            break;
+
+        default:
+            return;
     }
 
     if(!have_asnw)
     {
         have_asnw = reply_msg != NULL;
-        msg_destroy((msg_t*)message);
     }
     else
     {
